635e7e3e763f5e646a493f09c07a4134e824da34
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2015-11-29 04:51:11 -0500

    Add a group test

---
+++ Test/Case/AllTrackableTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * All Trackable plugin tests
+ */
+class AllTrackableTest extends PHPUnit_Framework_TestSuite {
+
+/**
+ * Suite method, defines tests for this suite.
+ *
+ * @return void
+ */
+	public static function suite() {
+		$Suite = new CakeTestSuite('All Trackable tests');
+		$path = CakePlugin::path('Trackable') . 'Test' . DS . 'Case' . DS;
+		$suite->addTestDirectoryRecursive($path);
+		return $Suite;
+	}
+}
